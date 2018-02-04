75f6b8822b157f9405358469affd58317216c78e
Benjamin Bearce <bbearce@Benjamins-MacBook-Pro.local>
2018-02-04 18:22:16 -0500

    changing port

--- app.py
+++ app.py
@@ -112,5 +112,5 @@ def Specific_Lagers(name):
     return render_template("Lagers/"+name)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 50000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
