1ff08ec26677382837fadbbb6d42eef98f627a48
Benjamin Bearce <bbearce@Benjamins-MacBook-Pro.local>
2018-02-24 20:33:23 -0500

    testing again

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template
-#from flask_script import Manager
+from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -10,7 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-#manager = Manager(app)
+manager = Manager(app)
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -31,86 +31,83 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-
-
-#
 # # Database - Start
-# class Fermentables(db.Model):
-#     __tablename__ = 'fermentables'
-#     id = db.Column(db.Integer, primary_key=True)
-#     ingredients = db.Column(db.String(64), unique=True)
-#     degree_linter = db.Column(db.String(5))
-#     value = db.Column(db.Integer)
-#     ppg = db.Column(db.Float)
-#     srm = db.Column(db.Float)
-#     ez_water_code = db.Column(db.Integer)
-#     flavor_profile = db.Column(db.Text)
-#     dp = db.Column(db.Integer)
-#     is_grain = db.Column(db.Integer)
-#
-#     def __repr__(self):
-#         return '<Role %r>' % self.ingredients
-#
-# class Hops(db.Model):
-#     __tablename__ = 'hops'
-#     id = db.Column(db.Integer, primary_key=True)
-#     hops = db.Column(db.String(64), unique=True)
-#     value = db.Column(db.Float)
-#     alphaLow = db.Column(db.Float)
-#     alphaHigh = db.Column(db.Float)
-#     flavorProfile = db.Column(db.String(200))
-#     possibleSubstitutions = db.Column(db.String(64))
-#     orgin = db.Column(db.String(64))
-#     storage = db.Column(db.String(64))
-#     additionalInformation_History = db.Column(db.Text)
-#
-#     def __repr__(self):
-#         return '<Role %r>' % self.hops
-#
-# class Yeast(db.Model):
-#     __tablename__ = 'yeast'
-#     id = db.Column(db.Integer, primary_key=True)
-#     yeastStrain = db.Column(db.String(64), unique=True)
-#     value = db.Column(db.Float)
-#     attenuation = db.Column(db.Float)
-#     temperatureRangeLow = db.Column(db.Integer)
-#     temperatureRangeHigh = db.Column(db.Integer)
-#     flocculation = db.Column(db.String(64))
-#     alcoholTolerancePercentLow = db.Column(db.Float)
-#     alcoholTolerancePercentHigh = db.Column(db.Float)
-#     flavorCharacteristics = db.Column(db.Text)
-#     recommendedStyles = db.Column(db.String(64))
-#     brewery = db.Column(db.String(64))
-#
-#     def __repr__(self):
-#         return '<Role %r>' % self.yeastStrain
-#
-# class Styles(db.Model):
-#     __tablename__ = 'styles'
-#     id = db.Column(db.Integer, primary_key=True)
-#     generalStyle = db.Column(db.String(64))
-#     styles = db.Column(db.String(64))
-#     OGRangeLow = db.Column(db.Float)
-#     OGRangeHigh = db.Column(db.Float)
-#     FGRangeLow = db.Column(db.Float)
-#     FGRangeHigh = db.Column(db.Float)
-#     bitterLow = db.Column(db.Float)
-#     bitterHigh = db.Column(db.Float)
-#     SRMRangeLow = db.Column(db.Float)
-#     SRMRangeHigh = db.Column(db.Float)
-#
-#     def __repr__(self):
-#         return '<Role %r>' % self.styles
-#
-# class gravity_correction_chart(db.Model):
-#     __tablename__ = 'gravity_correction_chart'
-#     id = db.Column(db.Integer, primary_key=True)
-#     temperature_F= db.Column(db.Integer)
-#     temperature_C= db.Column(db.Integer)
-#     add_SG = db.Column(db.Integer)
-#
-#     def __repr__(self):
-#         return '<Role %r>' % self.temperature_C
+class Fermentables(db.Model):
+    __tablename__ = 'fermentables'
+    id = db.Column(db.Integer, primary_key=True)
+    ingredients = db.Column(db.String(64), unique=True)
+    degree_linter = db.Column(db.String(5))
+    value = db.Column(db.Integer)
+    ppg = db.Column(db.Float)
+    srm = db.Column(db.Float)
+    ez_water_code = db.Column(db.Integer)
+    flavor_profile = db.Column(db.Text)
+    dp = db.Column(db.Integer)
+    is_grain = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Role %r>' % self.ingredients
+
+class Hops(db.Model):
+    __tablename__ = 'hops'
+    id = db.Column(db.Integer, primary_key=True)
+    hops = db.Column(db.String(64), unique=True)
+    value = db.Column(db.Float)
+    alphaLow = db.Column(db.Float)
+    alphaHigh = db.Column(db.Float)
+    flavorProfile = db.Column(db.String(200))
+    possibleSubstitutions = db.Column(db.String(64))
+    orgin = db.Column(db.String(64))
+    storage = db.Column(db.String(64))
+    additionalInformation_History = db.Column(db.Text)
+
+    def __repr__(self):
+        return '<Role %r>' % self.hops
+
+class Yeast(db.Model):
+    __tablename__ = 'yeast'
+    id = db.Column(db.Integer, primary_key=True)
+    yeastStrain = db.Column(db.String(64), unique=True)
+    value = db.Column(db.Float)
+    attenuation = db.Column(db.Float)
+    temperatureRangeLow = db.Column(db.Integer)
+    temperatureRangeHigh = db.Column(db.Integer)
+    flocculation = db.Column(db.String(64))
+    alcoholTolerancePercentLow = db.Column(db.Float)
+    alcoholTolerancePercentHigh = db.Column(db.Float)
+    flavorCharacteristics = db.Column(db.Text)
+    recommendedStyles = db.Column(db.String(64))
+    brewery = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<Role %r>' % self.yeastStrain
+
+class Styles(db.Model):
+    __tablename__ = 'styles'
+    id = db.Column(db.Integer, primary_key=True)
+    generalStyle = db.Column(db.String(64))
+    styles = db.Column(db.String(64))
+    OGRangeLow = db.Column(db.Float)
+    OGRangeHigh = db.Column(db.Float)
+    FGRangeLow = db.Column(db.Float)
+    FGRangeHigh = db.Column(db.Float)
+    bitterLow = db.Column(db.Float)
+    bitterHigh = db.Column(db.Float)
+    SRMRangeLow = db.Column(db.Float)
+    SRMRangeHigh = db.Column(db.Float)
+
+    def __repr__(self):
+        return '<Role %r>' % self.styles
+
+class gravity_correction_chart(db.Model):
+    __tablename__ = 'gravity_correction_chart'
+    id = db.Column(db.Integer, primary_key=True)
+    temperature_F= db.Column(db.Integer)
+    temperature_C= db.Column(db.Integer)
+    add_SG = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Role %r>' % self.temperature_C
 
 # Database - End
 
@@ -137,5 +134,5 @@ def Specific_Lagers(name):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 33507))
-    #manager.run()
-    app.run(host='0.0.0.0', port=port)
+    manager.run()
+    # app.run(host='0.0.0.0', port=port)
