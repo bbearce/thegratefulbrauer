3f395b5044ba88cb5bb8b16428b8649c7eb1ddbc
Benjamin <bbearce@mgh.harvard.edu>
2019-12-02 15:31:35 -0500

    asdf

--- app.py
+++ app.py
@@ -377,6 +377,7 @@ def brewculator():
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 33507))
     # manager.run()
-    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=port)
+    app.run()
     # !import code; code.interact(local=vars()) #Note this down as well
 
